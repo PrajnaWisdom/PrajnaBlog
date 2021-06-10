78922820efb061ec12f159da5a7fadc0c007e177
houkexin <kexinhou@webot.co>
2021-06-10 20:30:19 +0800

    feat

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import argparse
+
 from app.app import create_app
+from app.models.admin_user import AdminUser
+from app.config import ADMIN_ACCOUNT, ADMIN_PASSWORD
 
 
 app = create_app()
+
+
+def create_default_admin_user():
+    AdminUser.create(
+        account=ADMIN_ACCOUNT,
+        password=ADMIN_PASSWORD
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='init system')
+    parser.add_argument('--create_default_admin_user', action='store_true')
+    args = parser.parse_args()
+    if args.create_default_admin_user:
+        create_default_admin_user()
+
